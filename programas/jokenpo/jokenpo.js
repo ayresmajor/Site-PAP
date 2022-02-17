ebf3b70fe49bd7f7daaa2de978c59164b9fa104a
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-02-17 19:17:29 +0000

    jokenpo preparações

---
+++ programas/jokenpo/jokenpo.js
@@ -0,0 +1,9 @@
+function fechar(){
+    document.getElementById('fundo').style.display= 'none';
+    location.reload();
+}
+
+function iniciar() {
+    document.getElementById('fundo').style.display= 'block';
+    document.getElementById('euroiniciar').style.display = 'none';
+}
