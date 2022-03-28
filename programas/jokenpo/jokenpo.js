5b80000532cedd5ed7e6746876d80da61dafc484
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-03-28 18:40:49 +0100

    Começo do js

--- programas/jokenpo/jokenpo.js
+++ programas/jokenpo/jokenpo.js
@@ -7,3 +7,9 @@ function iniciar() {
     document.getElementById('fundo').style.display= 'block';
     document.getElementById('euroiniciar').style.display = 'none';
 }
+
+function startgame() {
+    loading = document.getElementById('loadingjokenpo');
+    button = document.getElementById('startgame')
+    loading.style.display= 'grid';
+}
