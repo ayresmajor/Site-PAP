7c40f18a03a3bb9898eb8829c723c9830348130e
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-04-01 23:05:24 +0100

    nada

--- Testes/teste.js
+++ Testes/teste.js
@@ -6,3 +6,7 @@ let njogos = 5
 for(var c = 1; c<= njogos; c++){
     var numeros = [randint(1,51),randint(1,51), randint(1,51), randint(1,51),randint(1,51), randint(1,51),randint(1,51)]
 }
+
+if (njogos == 0){
+    
+}
