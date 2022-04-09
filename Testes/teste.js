d0f29d95ee63561adde7ad2ace5e9f286a48121b
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-04-09 23:15:35 +0100

    sequencia de numero aleatorios sem repetição , traduzida para Js com sucesso

--- Testes/teste.js
+++ Testes/teste.js
@@ -1,35 +1,49 @@
 function randint(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 let num = [];
 let block = [];
 let block2 = [];
+let d = 0;
+let pos1 = 0;
+let pos2 = 0;
+let n = 0;
 
-  for (c = 0; c <= 6; c++) {
-    var pos1 = c - 2;
-    var  pos2 = c -2;
-   var n = randint(1, 6)
-    if(c > 0){
-      block.push(n);
-      if(c > 1){
-        while(block[pos1] == block[pos2]){
-          n = randint(1, 6);
-          block[pos2] = n
-        }
+for (let c = 0; c < 6; c++) {
+  pos1 = c - 2;
+  pos2 = c - 1;
+  console.log("")
+  console.log("Print do c :", c)
+  console.log("Print do pos1 e 2: ", pos1, pos2)
+  n = randint(1, 5);
+  console.log("Primeiro n: ", n )
+  if (c > 0) {
+    block.push(n);
+    if (c > 1) {
+      while (block[pos1] == block[pos2]) {
+        n = randint(1, 5);
+        block[pos2] = n;
       }
     }
-    if(block.length == 2){
-      block2 = block[0];
-    }else if(block.length > 2){
-      block2 = block.slice(0, c);
-      while( block2.includes(n)){
-        var d = block.indexOf(n);
-        n =randint(1, 6);
-        block[d] = n;
-      }
+    console.log("Primeiro Block: ", block)
+  }
+  if (block.length == 2) {
+    block2 = block[0];
+  } else if (block.length > 2) {
+    block2 = block.slice(0, c  - 1);
+    console.log("Block2: ", block2)
+    while (block2.includes(n)) {
+      d = block.indexOf(n);
+      n = randint(1, 5);
+      block[d] = n;
+      console.log("n modificado: ", n)
+      console.log("block modificado: ", block)
     }
   }
-  block.sort(function(a, b){return a - b});
-  console.log(block);
-
+}
+block.sort(function(a, b){return a - b})
+console.log('Valor final abaixo:')
+console.log(block)
