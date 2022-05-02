fcc214771b13386dcdc2e8ea813e4b345885360e
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-05-02 09:44:03 +0100

    teste top shadow bugado

--- script/script.js
+++ script/script.js
@@ -7,24 +7,27 @@ function randint(min, max) {
 let buttoncube = document.getElementById("buttoncube");
 window.addEventListener("scroll", function () {
   let value = Number(window.scrollY);
-  let gradient = value * (1/1700)
+  let gradient = value * (1/1700);
+  if (gradient > 1){
+    gradient = 1;
+  }
   document.body.style.backgroundColor = "rgb(var(--cbranco)," +  gradient + ")";
-  if(gradient > 1){
-    buttoncube.style.boxShadow= "rgb(var(--cbranco)) 0px -20px 60px 100px"
+  if(gradient == 1){
+    buttoncube.style.boxShadow= "rgb(var(--cbranco)) 0px -20px 60px 100px";
   }else{
-    buttoncube.style.boxShadow= "none"
+    buttoncube.style.boxShadow= "none";
   }
 });
 
 async function prograndom() {
-    let cube = document.querySelector(".cube")
-    cube.style.animation = "animate 1s linear infinite"
-    await sleep(1000)
-    cube.style.animation = "animate 0.5s linear infinite"
-    await sleep(1000)
-    cube.style.animation = "animate 0.2s linear infinite"
-    await sleep(500)
-    let random = randint(0,3)
+    let cube = document.querySelector(".cube");
+    cube.style.animation = "animate 1s linear infinite";
+    await sleep(1000);
+    cube.style.animation = "animate 0.5s linear infinite";
+    await sleep(1000);
+    cube.style.animation = "animate 0.2s linear infinite";
+    await sleep(500);
+    let random = randint(0,3);
     switch (random){
       case 0:
         window.location.href = "programas/jokenpo/index.html";
