1e12aac10e9bfb84dd7520ace622606b46ecf2ac
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-05-03 12:57:30 +0100

    Sim apareceu coisas que essse imbecil inventou
    Footer com gradiente do fundo

--- script/script.js
+++ script/script.js
@@ -6,7 +6,6 @@ function randint(min, max) {
 
 
 //Filtro de Browser
-alert(navigator.userAgent)
 let browser = navigator.userAgent.indexOf("Chrome")
 let version = navigator.userAgent[browser + 7]
 version += navigator.userAgent[browser + 8]
@@ -17,14 +16,17 @@ if(navigator.userAgent.indexOf("Chrome") == -1 || version < 99){
     window.alert("Para uma experiência completa utilize um browser de base Chromium como: Google Chrome, OperaGX/Opera ou Microsoft Edge. Caso esteja a utilizar um destes atualize para versão mais recente.")}
 
 let buttoncube = document.getElementById("buttoncube");
+let foot = document.getElementById("foot");
 window.addEventListener("scroll", function () {
   let value = Number(window.scrollY);
   let gradient = value * (1/1700);
   if (gradient > 1){
     gradient = 1;
   }
-  document.body.style.backgroundColor = "rgb(var(--cbranco)," +  gradient + ")";
+  foot.style.backgroundImage= `linear-gradient(to bottom,rgba(5, 5, 5,0), rgb(var(--cazules)))`
+  document.body.style.backgroundColor = `rgba(var(--cbranco),${gradient})`;
   if(gradient == 1){
+    foot.style.backgroundImage= `linear-gradient(to bottom,rgb(5, 5, 5), rgb(var(--cazules)))`
     buttoncube.style.boxShadow= "rgb(var(--cbranco)) 1px -100px 120px 0px";
   }else{
     buttoncube.style.boxShadow= "none";
